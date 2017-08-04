cd5c3048a127de3f82e4103ece24bcb1b8c6fe78
David Larlet <david@larlet.fr>
2017-08-04 11:38:21 -0400

    Multi-lines data for requests tests

--- tests/test_request.py
+++ tests/test_request.py
@@ -7,7 +7,19 @@ pytestmark = pytest.mark.asyncio
 async def test_request_parse_simple_get_response(app):
     req = Request(app)
     req.connection_made(app)
-    req.data_received(b'GET /feeds HTTP/1.1\r\nHost: localhost:1707\r\nUser-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:54.0) Gecko/20100101 Firefox/54.0\r\nAccept: */*\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\nOrigin: http://localhost:7777\r\nReferer: http://localhost:7777/\r\nDNT: 1\r\nConnection: keep-alive\r\n\r\n')  # noqa
+    req.data_received(
+        b'GET /feeds HTTP/1.1\r\n'
+        b'Host: localhost:1707\r\n'
+        b'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:54.0) '
+        b'Gecko/20100101 Firefox/54.0\r\n'
+        b'Accept: */*\r\n'
+        b'Accept-Language: en-US,en;q=0.5\r\n'
+        b'Accept-Encoding: gzip, deflate\r\n'
+        b'Origin: http://localhost:7777\r\n'
+        b'Referer: http://localhost:7777/\r\n'
+        b'DNT: 1\r\n'
+        b'Connection: keep-alive\r\n'
+        b'\r\n')
     assert req.method == 'GET'
     assert req.path == '/feeds'
     assert req.headers['Accept'] == '*/*'
@@ -16,7 +28,14 @@ async def test_request_parse_simple_get_response(app):
 async def test_request_parse_query_string(app):
     req = Request(app)
     req.connection_made(app)
-    req.data_received(b'GET /feeds?foo=bar&bar=baz HTTP/1.1\r\nHost: localhost:1707\r\nUser-Agent: HTTPie/0.9.8\r\nAccept-Encoding: gzip, deflate\r\nAccept: */*\r\nConnection: keep-alive\r\n\r\n')  # noqa
+    req.data_received(
+        b'GET /feeds?foo=bar&bar=baz HTTP/1.1\r\n'
+        b'Host: localhost:1707\r\n'
+        b'User-Agent: HTTPie/0.9.8\r\n'
+        b'Accept-Encoding: gzip, deflate\r\n'
+        b'Accept: */*\r\n'
+        b'Connection: keep-alive\r\n'
+        b'\r\n')
     assert req.path == '/feeds'
     assert req.query['foo'][0] == 'bar'
     assert req.query['bar'][0] == 'baz'
@@ -25,7 +44,14 @@ async def test_request_parse_query_string(app):
 async def test_request_parse_multivalue_query_string(app):
     req = Request(app)
     req.connection_made(app)
-    req.data_received(b'GET /feeds?foo=bar&foo=baz HTTP/1.1\r\nHost: localhost:1707\r\nUser-Agent: HTTPie/0.9.8\r\nAccept-Encoding: gzip, deflate\r\nAccept: */*\r\nConnection: keep-alive\r\n\r\n')  # noqa
+    req.data_received(
+        b'GET /feeds?foo=bar&foo=baz HTTP/1.1\r\n'
+        b'Host: localhost:1707\r\n'
+        b'User-Agent: HTTPie/0.9.8\r\n'
+        b'Accept-Encoding: gzip, deflate\r\n'
+        b'Accept: */*\r\n'
+        b'Connection: keep-alive\r\n'
+        b'\r\n')
     assert req.path == '/feeds'
     assert req.query['foo'] == ['bar', 'baz']
 
@@ -33,6 +59,16 @@ async def test_request_parse_multivalue_query_string(app):
 async def test_request_parse_POST_body(app):
     req = Request(app)
     req.connection_made(app)
-    req.data_received(b'POST /feed HTTP/1.1\r\nHost: localhost:1707\r\nUser-Agent: HTTPie/0.9.8\r\nAccept-Encoding: gzip, deflate\r\nAccept: application/json, */*\r\nConnection: keep-alive\r\nContent-Type: application/json\r\nContent-Length: 58\r\n\r\n{"link": "https://www.bastamag.net/spip.php?page=backend"}')  # noqa
+    req.data_received(
+        b'POST /feed HTTP/1.1\r\n'
+        b'Host: localhost:1707\r\n'
+        b'User-Agent: HTTPie/0.9.8\r\n'
+        b'Accept-Encoding: gzip, deflate\r\n'
+        b'Accept: application/json, */*\r\n'
+        b'Connection: keep-alive\r\n'
+        b'Content-Type: application/json\r\n'
+        b'Content-Length: 31\r\n'
+        b'\r\n'
+        b'{"link": "https://example.org"}')
     assert req.method == 'POST'
-    assert req.body == b'{"link": "https://www.bastamag.net/spip.php?page=backend"}'  # noqa
+    assert req.body == b'{"link": "https://example.org"}'
