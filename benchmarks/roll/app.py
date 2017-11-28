676ab3d3a3314c30cda5a466bde7307fa1a49303
Yohan Boniface <yb@enix.org>
2017-11-28 11:53:52 +0100

    Refactor benchs

--- benchmarks/roll/app.py
+++ benchmarks/roll/app.py
@@ -11,7 +11,32 @@ logging.getLogger('asyncio').setLevel(logging.CRITICAL)
 app = Roll()
 
 
-@app.route('/hello/{parameter}')
-async def hello(request, response, parameter):
-    response.json = {'message': f'Hello {parameter}'}
-    response.cookies.set(name='bench', value=request.cookies["test"])
+@app.route('/hello/minimal')
+async def minimal(request, response):
+    response.json = {'message': 'Hello, World!'}
+
+
+@app.route('/hello/with/{parameter}')
+async def parameter(request, response, parameter):
+    response.json = {'parameter': parameter}
+
+
+@app.route('/hello/cookie')
+async def cookie(request, response):
+    response.json = {'cookie': request.cookies['test']}
+    response.cookies.set(name='bench', value='value')
+
+
+@app.route('/hello/query')
+async def query(request, response):
+    response.json = {'query': request.query.get('query')}
+
+
+@app.route('/hello/full/with/{one}/and/{two}')
+async def full(request, response, one, two):
+    response.json = {
+        'parameters': f'{one} and {two}',
+        'query': request.query.get('query'),
+        'cookie': request.cookies['test'],
+    }
+    response.cookies.set(name='bench', value='value')
