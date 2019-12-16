d6b8963d04db845febd9f3ec10d13fd929c8ae44
David Larlet <david@larlet.fr>
2019-12-16 16:06:05 -0500

    Use ujson for starlette benchmark example

--- benchmarks/starlette/app.py
+++ benchmarks/starlette/app.py
@@ -1,10 +1,10 @@
 from starlette.applications import Starlette
-from starlette.responses import JSONResponse
+from starlette.responses import UJSONResponse
 from starlette.routing import Route
 
 
 async def minimal(request):
-    return JSONResponse({'message': 'Hello, World!'})
+    return UJSONResponse({'message': 'Hello, World!'})
 
 
 app = Starlette(debug=False, routes=[
